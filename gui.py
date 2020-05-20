ee72879e842d3e53dbff1d6741425a2373ba1897
Moosie6913 <Clark6913@hotmail.com>
2020-05-20 15:34:55 +1000

    add

--- gui.py
+++ gui.py
@@ -1,21 +1,108 @@
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+
 import sys
 
 
-def window():
-    xpos, width = 200,300
-    ypos, height = 200, 300
+class MainWindow(QMainWindow):
+
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        width, height = 600, 400
+        self.setGeometry(300,300,width,height)
+        self.setFixedSize(width,height)
+        self.setWindowTitle("Automation")
+        # self.setWindowIcon(QtWidgets.Qicon'/include/icon.png')
+
+        # Set up ActionBars
+        # User Preferences
+        user_preferences_action = QtWidgets.QAction("User Preferences",self)
+        user_preferences_action.setShortcut("Ctrl+U")
+        user_preferences_action.setStatusTip("Modify User Configured Settings")
+        user_preferences_action.triggered.connect(self.user_preferences)
+        #Log in Credentials
+        credential_action = QtWidgets.QAction("Credential Settings",self)
+        credential_action.setShortcut("Ctrl+Y")
+        credential_action.setStatusTip("Modify or Delete Login Information")
+        credential_action.triggered.connect(CredentialWindow.show())
+
+
+        self.statusBar()
+
+        mainMenu = self.menuBar()
+        settingsMenu = mainMenu.addMenu('&Settings')
+        settingsMenu.addAction(user_preferences_action)
+        settingsMenu.addAction(credential_action)
+
+
+
+        # Open up Home/Main Window
+        self.Home()
+
+    def Home(self):
+        btn = QtWidgets.QPushButton("Quit",self)
+        btn.clicked.connect(self.close_application)
+        btn.resize(btn.minimumSizeHint())
+        btn.move(520,370)
+        self.show()
+
+    def user_preferences(self):
+        pass
+
+
+    def close_application(self):
+        print("Closeing")
+        sys.exit()
+
+
+class CredentialWindow(QDialog):
+
+    def __init__(self):
+        super().__init__()
+        #self.win1 = QtWidget()
+        width, height = 600, 400
+        self.setGeometry(35, 350, width, height)
+        self.setFixedSize(width, height)
+        self.setWindowTitle("NewWindow")
+        self.test()
+        # def credential_settings(self):
+        #     pass
+
+    def test(self):
+        btn = QtWidgets.QPushButton("Quit", self)
+        btn.clicked.connect(self.blank)
+        btn.resize(btn.minimumSizeHint())
+        btn.move(520, 370)
+        self.show()
+
+    def blank(self):
+        pass
+
+
+def run():
     app = QApplication(sys.argv)
-    win = QMainWindow()
-    win.setGeometry(xpos, ypos, width, height)
-    win.setWindowTitle("TITLE TO BE CONFIRMED")
-
-    label = QtWidgets.QLabel(win)
-    label.setText("my first label")
-    label.move(50,50)
-
-    win.show()
+    win = MainWindow()
     sys.exit(app.exec_())
 
-window()
+
+if __name__ == '__main__':
+    run()
+
+# def window():
+#     xpos, width = 200,300
+#     ypos, height = 200, 300
+#     app = QApplication(sys.argv)
+#     win = QMainWindow()
+#     win.setGeometry(xpos, ypos, width, height)
+#     win.setWindowTitle("TITLE TO BE CONFIRMED")
+#
+#     label = QtWidgets.QLabel(win)
+#     label.setText("my first label")
+#     label.move(50,50)
+#
+#     win.show()
+#     sys.exit(app.exec_())
+#Clean looks or Plastique
+#window()
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import logging, os, time
+import logging, os, time, sys
 import gui
 from cryptography.fernet import Fernet
 from selenium import webdriver
@@ -8,37 +8,39 @@ from selenium.webdriver.support import expected_conditions as EC
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 logging.disable(logging.DEBUG)
 
+######################### CUSTOM DATA ###############################################
+targetURL = 'https://ebs.worley.com/OA_HTML/OA.jsp?OAFunc=OAHOMEPAGE'
+
 
 class Encryption:
     # This class handles tasks related to encryption and decryption
-    def __init__(self, item, filename):
-        self.item = item
-        self.filename = filename
+    def __init__(self):
+        pass
 
-    def write(self):
+    def write(self,item,filename):
         # Writing and storing credentials to file
-        # Returns null
-        logging.info('Saving user data...')
-        logging.debug('%s key: %s' % (self.filename, self.item))
-        path = "./include/data/" + self.filename
+        # Returns None
+        logging.debug('Saving user data...')
+        logging.debug('%s key: %s' % (filename, item))
+        path = "./include/data/" + filename
         file = open(path, 'wb')
-        file.write(self.item)
+        file.write(item)
         file.close()
 
-    def read(self):
+    def read(self,filename):
         # Reading credentials from stored file
         # Returns (encrypted item)
-        logging.info('Loading user data...')
-        path = "./include/data/" + self.filename
+        logging.debug('Loading user data...')
+        path = "./include/data/" + filename
         file = open(path, 'rb')
         item_encrypt = file.read()
         file.close()
-        logging.debug('%s key: %s' % (self.filename, item_encrypt))
+        logging.debug('%s key: %s' % (filename, item_encrypt))
         return item_encrypt
 
-    def clear(self, pair):
+    def clear(self):
         # Clears ALL stored data user email and password data
-        # Returns null
+        # Returns None
         try:  # if it exists removed the data
             logging.info('Deleting all stored log in details.')
             os.remove("./include/data/key.key")
@@ -48,7 +50,7 @@ class Encryption:
             os.remove("./include/data/pair4.pair")
             logging.info('Successfully deleted.')
         except:  # if no data exists or it error proceed
-            logging.error('No user data found... Continuing.')
+            logging.error('No stored user data found... Continuing...')
             pass
 
     def save(self, dual=False):
@@ -74,11 +76,11 @@ class Encryption:
             dual_user_encrypt = None
             dual_password_encrypt = None
         # Store user and password pair for access next time
-        Encryption(user_encrypt, 'pair1.pair').write()
-        Encryption(password_encrypt, 'pair2.pair').write()
+        Encryption().write(user_encrypt, 'pair1.pair')
+        Encryption().write(password_encrypt, 'pair2.pair')
         if dual:
-            Encryption(dual_user_encrypt, 'pair3.pair').write()
-            Encryption(dual_password_encrypt, 'pair4.pair').write()
+            Encryption().write(dual_user_encrypt, 'pair3.pair')
+            Encryption().write(dual_password_encrypt, 'pair4.pair')
         logging.info('Credentials stored successfully')
 
         logging.debug('Encrypted Username: %s' % user_encrypt)
@@ -100,11 +102,11 @@ class Encryption:
 
         # Load encrypted credentials
         logging.info('Loading existing credentials...')
-        user_encrypt = Encryption(None, 'pair1.pair').read()
-        password_encrypt = Encryption(None, 'pair2.pair').read()
+        user_encrypt = Encryption().read('pair1.pair')
+        password_encrypt = Encryption().read('pair2.pair')
         if dual:
-            dual_user_encrypt = Encryption(None, 'pair3.pair').read()
-            dual_password_encrypt = Encryption(None, 'pair4.pair').read()
+            dual_user_encrypt = Encryption().read('pair3.pair')
+            dual_password_encrypt = Encryption().read('pair4.pair')
         else:  # else return nulls
             dual_user_encrypt = None
             dual_password_encrypt = None
@@ -116,22 +118,148 @@ class Encryption:
         return [f, user_encrypt, password_encrypt, dual_user_encrypt, dual_password_encrypt]
 
 class Authentication:
+    # This class handles login
 
-    def __init__(self):
+    def __init__(self,driver,f,user1,pass1,user2,pass2):
+        self.driver = driver
+        self.f = f
+        self.user1 = user1
+        self.pass1 = pass1
+        self.user2 = user2
+        self.pass2 = pass2
+
+    def microsoftlogin(self, targetURL):
+        # Microsoft log on screen with MFA/2FA
+        logging.info('Microsoft Authentication required...')
+        self.driver.get(targetURL)
+
+        # Input Username
+        logging.debug('Entering Email')
+        user_elem = Navigate(self.driver, 'i0116', 'ID').loadelem(10)
+        user_elem.send_keys(self.f.decrypt(self.user1).decode())
+        Navigate(self.driver, 'idSIButton9', 'ID').click(2)
+
+        # Input Password
+        logging.debug('Entering Password')
+        pass_elem = Navigate(self.driver, 'i0118', 'ID').loadelem(10)
+        pass_elem.send_keys(self.f.decrypt(self.pass1).decode())
+        Navigate(self.driver, 'idSIButton9', 'ID').click(2)
+
+        # try:  # Check to see if password is incorrect/Failed
+        #     pass_error = Navigate(self.driver, 'passwordError', 'ID').loadelem(1)
+        #     logging.error("Password is incorrect, please enter correct credentials...")
+        #     input('Press any key to close...')
+        # except:  # Do nothing as it worked
+        #     logging.debug("Password accepted")
+        #     pass
+        # if:
+        #     self.driver.quit()
+        #     sys.exit()
+        #TODO add password incorrect if ID='passwordError' Appears
+
+        # MFA/2FA stage
+        logging.info('Waiting for you to complete authentication step on your mobile. Waiting up to 60secs.')
+        #TODO add different types of MFA, not just moblie (ADD CODE)
+        elem = Navigate(self.driver, 'KmsiCheckboxField', 'ID').loadelem(60)
+        elem.click()
+        logging.info('Authentication successful!')
+        Navigate(self.driver, 'idSIButton9', 'ID').click(2)
+
+        # TODO incorportate this check if authenticaiton is even required
+        # /html/body/div/form[1]/div/div/div[1]/div[2]/div[1]/img
+        # if this exists, login is required
+
+    def worley(self):
         pass
 
+    def ecr(self):
+        pass
+
+
 class Navigate:
-    # Basic Web fuctions with handleing xpath, ID, linktext
-    def __init__(self,driver,elem):
+    # Basic Web factions with handling xpath, ID, linktext
 
+    def __init__(self,driver,item,elemtype):
         self.driver = driver
-        self.elem = elem
-        #click & wait time
-        #click & wait for element
+        self.item = item
+        self.elemtype = elemtype
+
+    def loadelem(self,maxtime=int(30),interact=False):
+        # Waits for element to load
+        # returns (element)
+
+        try:
+            if str(self.elemtype.upper()) == 'ID':
+                element = WebDriverWait(self.driver,maxtime).until(
+                    EC.presence_of_element_located((By.ID, self.item)))
+                return element
+            elif str(self.elemtype.upper()) == 'XPATH':
+                pass  # TODO Set LOAD XPATH up
+            elif str(self.elemtype.upper()) == 'LINKTEXT':
+                pass  # TODO Set LOAD LINKTEXT up
+            else:
+                logging.error("Click&Load Method Failed ERROR - Element type is incorrect")
+                return None
+        except:
+            logging.error('Click&Load Method Failed ERROR - Element did not load\n\rClosing website now')
+            input('Press any key to close...')
+            quit()
 
 
-# make driver as a normal function then pass into navigate
-def pagedriver():
+
+    def click(self,delay=int(1)):
+        # Finds and clicks element based on ID, XPATH or LINKTEXT
+        # returns None
+
+        # Locate Element
+        try:
+            if str(self.elemtype.upper()) == 'ID':
+                element = self.driver.find_element_by_id(self.item)
+            elif str(self.elemtype.upper()) == 'XPATH':
+                element = self.driver.find_element_by_xpath(self.item)
+            elif str(self.elemtype.upper()) == 'LINKTEXT':
+                element = self.driver.find_element_by_link_text(self.item)
+            else:
+                logging.error("Click Method Failed ERROR - Element type is incorrect")
+                return None
+
+            element.click()# Click Element
+            time.sleep(delay)  # Delay before next action
+        except:
+            logging.error('Click Method Failed ERROR - Element did not load\n\rClosing website now')
+            input('Press any key to close...')
+            quit()
+
+
+def getdriver():
     PATH_DRIVER = os.path.join(os.getcwd(), r'include\browser\driver\firefoxdriver.exe') # Currently using OS firefox.
     driver = webdriver.Firefox(executable_path=PATH_DRIVER)  # , firefox_binary=firefox_binary)
     return driver
+
+
+#MAIN
+# Ask if new or current credentials
+print('Select from the following options:\n[1] - Input New Login Details\n[2] - Use Existing')
+x = input('>>> ')
+
+dual = False
+
+if x == '1':  # if new credentials are reburied
+
+    logging.info('Option 1 Selected: Override existing credentials? ')
+    x = input('Override existing credentials? [Y/N]\n')
+    if x.upper() == 'Y':  # If override confirm
+        print("Please enter your network log in details... e.g \'connor.clark@worleyparsons.com\' ")
+        Encryption().clear()
+        f, user_encrypt, password_encrypt, dual_user_encrypt, dual_password_encrypt = Encryption().save(dual)
+        # TODO: Add in confirmation of details
+    else: # abort program
+        input('Press any key to close...')
+        sys.exit()
+
+else: # if using previous credentials
+    f, user_encrypt, password_encrypt, dual_user_encrypt, dual_password_encrypt = Encryption().load(dual)
+
+driver = getdriver()
+Authentication(driver,f,user_encrypt,password_encrypt,dual_user_encrypt,dual_password_encrypt).microsoftlogin(targetURL)
+
