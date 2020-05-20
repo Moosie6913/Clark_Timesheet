92822ac7f55f70994b45b35e2ce21d0316bee9ad
Moosie6913 <Clark6913@hotmail.com>
2020-05-21 09:02:34 +1000

    add

--- gui.py
+++ gui.py
@@ -2,7 +2,7 @@ from PyQt5 import QtWidgets
 #from PyQt5.QtGui import *
 #from PyQt5.QtCore import *
 from PyQt5.QtWidgets import QApplication, QMainWindow
-import breeze_resources
+#import breeze_resources
 import sys
 
 
